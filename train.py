32a3c0bc7fed89bb54edf7a72d334c96fc97f8d4
Maral katebi <marall@live.com>
2026-08-25 20:34:01 +0330

    added train file and trained weights

---
+++ train.py
@@ -0,0 +1,21 @@
+from stable_baselines3 import PPO
+from game.environment import PlatformerEnv
+
+
+env = PlatformerEnv(
+    max_episode_steps=1000,
+    seed=42
+)
+
+model = PPO(
+    "MlpPolicy",
+    env,
+    verbose=1,
+    seed=42,
+)
+
+model.learn(total_timesteps=10_000)
+
+model.save("ppo_platformer")
+
+env.close()
